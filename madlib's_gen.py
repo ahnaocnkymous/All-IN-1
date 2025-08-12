90fc2ba6ffa8897620bd3e96d4a0049f1a873eb6
ahnaocnkymous <139701313+ahnaocnkymous@users.noreply.github.com>
2025-08-12 08:36:57 +0530

    madlib's
    replace text as you like this could be fun how to pick and think the random things in your mind

---
+++ madlib's_gen.py
@@ -0,0 +1,28 @@
+print("I'm gonna be the King of Hackers/Programmer!\nI'm gonna surpass my limits right here, right now~")
+with open('story.txt','r') as f:
+    story = f.read()
+
+words = set()
+start_of_word = -1
+
+target_start = "<"
+target_end = ">"
+
+for i, char in enumerate(story):
+    if char == target_start:
+        start_of_word = i
+    if char == target_end and start_of_word != -1:
+        word = story[start_of_word: i + 1]
+        words.add(word)
+        start_of_word  = -1
+
+# print(words) 
+answers = {}
+
+for word in words:
+    answer = input("Enter a word for "+word+": ")
+    answers[word] = answer
+
+for word in words:
+    story = story.replace(word, answers[word])
+print(story)
